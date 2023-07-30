7dc80dd79410a997b0a94f5bf57bb1be42355417
Денис Сушков <ga.akam@yandex.ru>
2023-07-30 14:52:12 +0300

    latest

--- app/schemas/charity_project.py
+++ app/schemas/charity_project.py
@@ -24,6 +24,7 @@ class CharityProjectBase(BaseModel):
             raise ValueError('Имя не может быть пустым!')
         return value
 
+
 class CharityProjectCreate(CharityProjectBase):
     """Схема создания благотварительных проектов."""
     pass
